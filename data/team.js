ec4f99d5996ac4ba03f9b35517901b733126e993
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-20 15:59:43 +0600

    refactor: extract team data to a separate file

---
+++ data/team.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+const team = [{
+  name: 'Аян Енбекбай',
+  role: 'Сооснователь, CEO',
+  photoUrl: '/about/ayan.jpg',
+  links: [{
+    title: 'Facebook',
+    href: 'https://facebook.com/yenbekbay',
+  }, {
+    title: 'Github',
+    href: 'https://github.com/yenbekbay',
+  }],
+}, {
+  name: 'Бинур Конарбай',
+  role: 'Сооснователь, CTO',
+  photoUrl: '/about/binur.jpg',
+  links: [{
+    title: 'Facebook',
+    href: 'https://facebook.com/binchik',
+  }, {
+    title: 'Github',
+    href: 'https://github.com/binchik',
+  }],
+}, {
+  name: 'Айрис',
+  role: 'Немецкий шпиц',
+  photoUrl: '/about/ayris.jpg',
+}];
+
+export default team;
--- pages/about/index.js
+++ pages/about/index.js
@@ -10,34 +10,7 @@ import {
   Separator,
   TeamList,
 } from '../../components';
-
-const team = [{
-  name: 'Аян Енбекбай',
-  role: 'Сооснователь, CEO',
-  photoUrl: '/about/ayan.jpg',
-  links: [{
-    title: 'Facebook',
-    href: 'https://facebook.com/yenbekbay',
-  }, {
-    title: 'Github',
-    href: 'https://github.com/yenbekbay',
-  }],
-}, {
-  name: 'Бинур Конарбай',
-  role: 'Сооснователь, CTO',
-  photoUrl: '/about/binur.jpg',
-  links: [{
-    title: 'Facebook',
-    href: 'https://facebook.com/binchik',
-  }, {
-    title: 'Github',
-    href: 'https://github.com/binchik',
-  }],
-}, {
-  name: 'Айрис',
-  role: 'Немецкий шпиц',
-  photoUrl: '/about/ayris.jpg',
-}];
+import team from '../../data/team';
 
 /* eslint-disable max-len */
 const ContactPage = (): React$Element<any> => (
