0f7d4c5ce74c54f76900e4013d266d052c64c038
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-20 17:37:04 +0600

    refactor(StickyNavigation): simplify and extract `TopNavigation` component

--- components/StickyNavigation.js
+++ components/StickyNavigation.js
@@ -1,20 +1,11 @@
 /* @flow */
 
-/* heavily borrows from https://github.com/KyleAMathews/react-headroom */
-
-// eslint-disable-next-line import/no-extraneous-dependencies, import/extensions
-import { config } from 'config';
-import { Link } from 'react-router';
 import autobind from 'autobind-decorator';
-import cx from 'classnames';
 import prefixAll from 'inline-style-prefixer/static';
 import raf from 'raf';
 import React, { Component } from 'react';
 
-import NavigationLink from './NavigationLink';
-import ResponsiveImage from './ResponsiveImage';
-
-const { menuLinks } = config;
+import TopNavigation from './TopNavigation';
 
 const getViewportHeight = (): number => (
   window.innerHeight
@@ -45,9 +36,7 @@ const getScrollY = (): number => {
 };
 
 type State = {
-  state: 'unfixed',
-  hidden: boolean,
-  mobileMenuVisible: boolean,
+  pinned: boolean,
 };
 
 export default class StickyNavigation extends Component {
@@ -57,14 +46,16 @@ export default class StickyNavigation extends Component {
   lastKnownScrollY = 0;
   ticking = false;
   state = {
-    state: 'unfixed',
-    hidden: true,
-    mobileMenuVisible: false,
+    pinned: false,
   };
   /* eslint-enable react/sort-comp */
 
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
+  }
+
+  shouldComponentUpdate(nextProps: any, nextState: State) {
+    return nextState.pinned !== this.state.pinned;
   }
 
   componentWillUnmount() {
@@ -77,38 +68,6 @@ export default class StickyNavigation extends Component {
       this.ticking = true;
       raf(this.update);
     }
-  }
-
-  unpin() {
-    if (this.state.state === 'unpinned') return;
-
-    this.setState({
-      hidden: true,
-      mobileMenuVisible: false,
-    }, () => {
-      setTimeout(() => {
-        this.setState({ state: 'unpinned' });
-      }, 0);
-    });
-  }
-
-  pin() {
-    if (this.state.state === 'pinned') return;
-
-    this.setState({
-      hidden: false,
-      state: 'pinned',
-    });
-  }
-
-  unfix() {
-    if (this.state.state === 'unfixed') return;
-
-    this.setState({
-      hidden: true,
-      state: 'unfixed',
-      mobileMenuVisible: false,
-    });
   }
 
   @autobind
@@ -125,15 +84,12 @@ export default class StickyNavigation extends Component {
       const sideNav = document.getElementById('side-nav');
       const sideNavBottom = sideNav.offsetTop + sideNav.offsetHeight;
 
-      if (currentScrollY <= sideNavBottom) {
-        // we're at the top and not fixed yet
-        this.unfix();
-      } else if (distanceScrolled > 0) {
-        // we're past the header and scrolling down
-        this.unpin();
+      if (currentScrollY <= sideNavBottom || distanceScrolled > 0) {
+        // we're at the top or past the header and scrolling down
+        this.setState({ pinned: false });
       } else if (distanceScrolled < -5) {
         // we're scrolling up
-        this.pin();
+        this.setState({ pinned: true });
       }
     }
 
@@ -142,84 +98,15 @@ export default class StickyNavigation extends Component {
   }
 
   render(): React$Element<any> {
-    const { hidden, mobileMenuVisible } = this.state;
-
     return (
-      <nav
-        className="z-999 w-100 fixed top-0 left-0"
+      <TopNavigation
+        className="fixed z-999 w-100 top-0 left-0"
         style={prefixAll({
           transition: 'transform 0.2s ease',
-          transform: `translateY(${hidden ? '-100%' : '0'})`,
+          transform: `translateY(${this.state.pinned ? '0' : '-100%'})`,
         })}
-      >
-        <div
-          // eslint-disable-next-line max-len
-          className="z-1 relative pa3 ph5-ns bg-white flex items-center justify-between"
-        >
-          <a
-            className="link dim dark-gray f4 dib db dn-ns pointer"
-            onClick={() => {
-              this.setState({
-                mobileMenuVisible: !mobileMenuVisible,
-              });
-            }}
-          >
-            {mobileMenuVisible ? 'Закрыть' : 'Меню'}
-          </a>
-          <Link
-            to="/"
-            className="link dim"
-          >
-            <ResponsiveImage
-              srcSet={[
-                require('../static/logo.png'),
-                require('../static/logo@2x.png'),
-                require('../static/logo@3x.png'),
-              ]}
-              alt="Anvilabs Logo"
-              className="dib w-auto"
-              style={{ height: '3rem' }}
-            />
-          </Link>
-          <div className="dn db-ns">
-            {menuLinks.map((
-              { to, title }: { to: string, title: string },
-              idx: number,
-            ) => (
-              <NavigationLink
-                key={to}
-                to={to}
-                title={title}
-                className={cx(
-                  'f5 f4-l dib',
-                  (idx < menuLinks.length - 1) && 'mr3 mr4-ns',
-                )}
-              />
-            ))}
-          </div>
-        </div>
-        <div
-          className="relative pa3 db dn-ns w-100 bg-white"
-          style={{
-            maxHeight: mobileMenuVisible ? '100%' : 0,
-            opacity: mobileMenuVisible ? 1 : 0,
-            visibility: mobileMenuVisible ? 'visible' : 'hidden',
-            transform: `translateY(${mobileMenuVisible ? '0' : '-2em'})`,
-            transition: 'transform 0.2s ease, opacity 0.2s ease, visibility 0s',
-          }}
-        >
-          {menuLinks.map((
-            { to, title }: { to: string, title: string },
-          ) => (
-            <NavigationLink
-              key={to}
-              to={to}
-              title={title}
-              className="db pv2 f4"
-            />
-          ))}
-        </div>
-      </nav>
+        mobileMenuVisible={false}
+      />
     );
   }
 }
---
+++ components/TopNavigation.js
@@ -0,0 +1,123 @@
+/* @flow */
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/extensions
+import { config } from 'config';
+import { Link } from 'react-router';
+import cx from 'classnames';
+import React, { Component } from 'react';
+
+import NavigationLink from './NavigationLink';
+import ResponsiveImage from './ResponsiveImage';
+
+const { menuLinks } = config;
+
+type Props = {
+  mobileMenuVisible: boolean,
+  className?: string,
+  style?: string,
+};
+type State = {
+  mobileMenuVisible: boolean,
+};
+
+export default class TopNavigation extends Component {
+  props: Props;
+  state: State;
+
+  static defaultProps = {
+    mobileMenuVisible: false,
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      mobileMenuVisible: props.mobileMenuVisible,
+    };
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.mobileMenuVisible !== this.state.mobileMenuVisible) {
+      this.setState({
+        mobileMenuVisible: nextProps.mobileMenuVisible,
+      });
+    }
+  }
+
+  render(): React$Element<any> {
+    const { className, style } = this.props;
+    const { mobileMenuVisible } = this.state;
+
+    return (
+      <nav className={className} style={style}>
+        <div
+          // eslint-disable-next-line max-len
+          className="z-1 relative pa3 ph5-ns bg-white flex items-center justify-between"
+        >
+          <a
+            className="link dim dark-gray f4 dib db dn-ns pointer"
+            onClick={() => {
+              this.setState({
+                mobileMenuVisible: !mobileMenuVisible,
+              });
+            }}
+          >
+            {mobileMenuVisible ? 'Закрыть' : 'Меню'}
+          </a>
+          <Link
+            to="/"
+            className="link dim"
+          >
+            <ResponsiveImage
+              srcSet={[
+                require('../static/logo.png'),
+                require('../static/logo@2x.png'),
+                require('../static/logo@3x.png'),
+              ]}
+              alt="Anvilabs Logo"
+              className="dib w-auto"
+              style={{ height: '3rem' }}
+            />
+          </Link>
+          <div className="dn db-ns">
+            {menuLinks.map((
+              { to, title }: { to: string, title: string },
+              idx: number,
+            ) => (
+              <NavigationLink
+                key={to}
+                to={to}
+                title={title}
+                className={cx(
+                  'f5 f4-l dib',
+                  (idx < menuLinks.length - 1) && 'mr3 mr4-ns',
+                )}
+              />
+            ))}
+          </div>
+        </div>
+        <div
+          className="relative pa3 db dn-ns w-100 bg-white"
+          style={{
+            maxHeight: mobileMenuVisible ? '100%' : 0,
+            opacity: mobileMenuVisible ? 1 : 0,
+            visibility: mobileMenuVisible ? 'visible' : 'hidden',
+            transform: `translateY(${mobileMenuVisible ? '0' : '-2em'})`,
+            transition: 'transform 0.2s ease, opacity 0.2s ease, visibility 0s',
+          }}
+        >
+          {menuLinks.map((
+            { to, title }: { to: string, title: string },
+          ) => (
+            <NavigationLink
+              key={to}
+              to={to}
+              title={title}
+              className="db pv2 f4"
+            />
+          ))}
+        </div>
+      </nav>
+    );
+  }
+}
