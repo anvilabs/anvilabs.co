c0f81ed5dbbbafc58ea04421f06c34e846a1240a
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-22 23:19:07 +0600

    test: set up tests with jest

---
+++ components/__tests__/Analytics-test.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import renderer from 'react-test-renderer';
+
+import Analytics from '../Analytics';
+
+describe('Analytics', () => {
+  it('returns a script tag when passed a valid Segment write key', () => {
+    const component = renderer.create(
+      <Analytics writeKey="123" />,
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('returns null when passed an empty Segment write key', () => {
+    const component = renderer.create(
+      <Analytics writeKey="" />,
+    );
+    expect(component.toJSON()).toBeNull();
+  });
+});
+
