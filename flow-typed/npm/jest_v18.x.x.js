9d3c78e1a83ea933f539638602fb4348c7b74b62
Ayan Yenbekbay <ayan.yenb@gmail.com>
2017-02-12 01:58:56 +0600

    chore(flow): update flow-typed definitions

--- flow-typed/npm/jest_v18.x.x.js
+++ flow-typed/npm/jest_v18.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 269340ea4ca4555368bd144efc3753b4
-// flow-typed version: 2b78d73874/jest_v17.x.x/flow_>=v0.33.x
+// flow-typed signature: e49570b0f5e396c7206dda452bd6f004
+// flow-typed version: 1590d813f4/jest_v18.x.x/flow_>=v0.33.x
 
 type JestMockFn = {
   (...args: Array<any>): any,
@@ -190,13 +190,26 @@ type JestExpectType = {
    */
   toHaveBeenCalledWith(...args: Array<any>): void,
   /**
+   * Check that an object has a .length property and it is set to a certain
+   * numeric value.
+   */
+  toHaveLength(number: number): void,
+  /**
+   *
+   */
+  toHaveProperty(propPath: string, value?: any): void,
+  /**
    * Use .toMatch to check that a string matches a regular expression.
    */
   toMatch(regexp: RegExp): void,
   /**
+   * Use .toMatchObject to check that a javascript object matches a subset of the properties of an object.
+   */
+  toMatchObject(object: Object): void,
+  /**
    * This ensures that a React component matches the most recent snapshot.
    */
-  toMatchSnapshot(): void,
+  toMatchSnapshot(name?: string): void,
   /**
    * Use .toThrow to test that a function throws when it is called.
    */
@@ -369,6 +382,13 @@ declare var it: {
    * @param {Function} Test
    */
   skip(name: string, fn?: Function): ?Promise<void>,
+  /**
+   * Run the test concurrently
+   *
+   * @param {string} Name of Test
+   * @param {Function} Test
+   */
+  concurrent(name: string, fn?: Function): ?Promise<void>,
 };
 declare function fit(name: string, fn: Function): ?Promise<void>;
 /** An individual test unit */
@@ -388,6 +408,12 @@ declare var expect: {
   (value: any): JestExpectType,
   /** Add additional Jasmine matchers to Jest's roster */
   extend(matchers: {[name:string]: JestMatcher}): void,
+  assertions(expectedAssertions: number): void,
+  any(value: mixed): JestAsymmetricEqualityType,
+  anything(): void,
+  arrayContaining(value: Array<mixed>): void,
+  objectContaining(value: Object): void,
+  stringMatching(value: string): void,
 };
 
 // TODO handle return type
@@ -408,6 +434,7 @@ declare var jasmine: {
   arrayContaining(value: Array<mixed>): void,
   clock(): JestClockType,
   createSpy(name: string): JestSpyType,
+  createSpyObj(baseName: string, methodNames: Array<string>): {[methodName: string]: JestSpyType},
   objectContaining(value: Object): void,
   stringMatching(value: string): void,
 }
